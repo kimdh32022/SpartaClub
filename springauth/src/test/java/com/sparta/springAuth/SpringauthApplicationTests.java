a08040d07b9ce90c328838031205aed81a623887
kimdh32022 <kimdh32022@naver.com>
2026-01-12 16:15:11 +0900

    init commit

---
+++ springauth/src/test/java/com/sparta/springAuth/SpringauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
