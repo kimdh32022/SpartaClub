9a34f978c525a1afd811130cd2a89bd44ced352d
kimdh32022 <kimdh32022@naver.com>
2026-01-15 19:29:49 +0900

    다시 복구중

--- spring-resttemplate-client/src/test/java/com/sparta/springresttemplateclient/SpringResttemplateClientApplicationTests.java
+++ spring-resttemplate-client/src/test/java/com/sparta/springresttemplateclient/SpringResttemplateClientApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringResttemplateClientApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
