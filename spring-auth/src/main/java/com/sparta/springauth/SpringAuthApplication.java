aaf1548732ad5c12089b9d6664c4517dfdd3e26f
kimdh32022 <kimdh32022@naver.com>
2026-01-15 12:57:48 +0900

    JWT 로그인 구현 완료

---
+++ spring-auth/src/main/java/com/sparta/springauth/SpringAuthApplication.java
@@ -0,0 +1,16 @@
+package com.sparta.springauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import javax.swing.*;
+
+@SpringBootApplication //(exclude = SecurityAutoConfiguration.class) Spring Security 인증 기능 제외
+public class SpringAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthApplication.class, args);
+    }
+
+}
