11b6b44723948015b8134e722b1d403ab3f8cbce
kimdh32022 <kimdh32022@naver.com>
2026-02-04 16:34:38 +0900

    유저 엔티티 추가

--- memoTest/src/main/java/com/sparta/memotest/entity/User.java
+++ memoTest/src/main/java/com/sparta/memotest/entity/User.java
@@ -1,4 +1,34 @@
 package com.sparta.memotest.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userid;
+
+    // 아이디
+    @Column(nullable = false, unique = true)
+    private String idname;
+
+    //비밀번호
+    @Column(nullable = false)
+    private String password;
+
+    // 이메일
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Memo memo;
+
+
+
 }
